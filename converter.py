150608d109138ab998f0aefc27c1809a2c58b029
Nikodem <nikodemszwast@gmail.com>
2023-05-21 21:47:11 +0200

    change algorithm to be faster and make more pixelated images

--- converter.py
+++ converter.py
@@ -1,22 +1,38 @@
 from PIL import Image
-from sklearn.cluster import KMeans
-import numpy as np
 import os
 
-input_image = Image.open(os.path.join("data", "to_convert", "test.jpg"))
+def pixelate(name, block_size):
+    image_path = os.path.join("data", "to_convert", f"{name}.jpg")
+    input_image = Image.open(image_path)
+    # so it fits GAN model
+    input_image = input_image.resize((512, 512))
+    width, height = input_image.size
 
-input_array = np.array(input_image)
-pixels = input_array.reshape(-1, 3)
+    num_blocks_x = width // block_size
+    num_blocks_y = height // block_size
 
-k = 6  # needs more testing
-kmeans = KMeans(n_clusters=k, random_state=33).fit(pixels)
+    output_image = Image.new('RGB', (width, height))
 
-labels = kmeans.labels_
-labels = labels.reshape(input_array.shape[:2])
+    for block_x in range(num_blocks_x):
+        for block_y in range(num_blocks_y):
+            x_start = block_x * block_size
+            y_start = block_y * block_size
+            x_end = x_start + block_size
+            y_end = y_start + block_size
 
-output_array = np.zeros_like(input_array)
-for i in range(k):
-    output_array[labels == i] = kmeans.cluster_centers_[i]
+            block_pixels = []
+            for x in range(x_start, x_end):
+                for y in range(y_start, y_end):
+                    pixel = input_image.getpixel((x, y))
+                    block_pixels.append(pixel)
 
-output_image = Image.fromarray(output_array)
-output_image.save(os.path.join("data", "converted", "converted_test.jpg"))
+            average_color = tuple(int(sum(channel) / len(block_pixels)) for channel in zip(*block_pixels))
+
+            for x in range(x_start, x_end):
+                for y in range(y_start, y_end):
+                    output_image.putpixel((x, y), average_color)
+
+    output_image.save(os.path.join("data", "to_convert", f'pixelated_{name}.png'))
+
+name = "2244"
+pixelate(name, 10)
