348a3291864d318eebb88e62b50d0e4e1e0dd800
Nikodem <nikodemszwast@gmail.com>
2023-06-09 21:57:16 +0200

    add functionality to automatically move converted files to 'temp' direcotry

--- converter.py
+++ converter.py
@@ -1,8 +1,12 @@
 from PIL import Image
 import os
+import shutil
+
 
 def pixelate(name, block_size):
     image_path = os.path.join("data", "to_convert", f"{name}.jpg")
+    # like a 2nd bin, to move images after processing them but not deleting
+    temp_image_path = os.path.join("data", "temp", f"{name}.jpg")
     input_image = Image.open(image_path)
     # so it fits GAN model
     input_image = input_image.resize((512, 512))
@@ -33,6 +37,8 @@ def pixelate(name, block_size):
                     output_image.putpixel((x, y), average_color)
 
     output_image.save(os.path.join("data", "converted", f'pixelated_{name}_{block_size}.png'))
+    shutil.move(image_path, temp_image_path)
+
 
 def convert_all(block_size):
     data_dir_path = os.path.join("data", "to_convert")
